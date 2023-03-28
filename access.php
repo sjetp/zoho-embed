6fefe69ce2506ea31401c2b7588e0ada12ed0ec9
sjetp <sjetpaul23@gmail.com>
2023-03-28 11:32:49 +0530

    no consent

--- access.php
+++ access.php
@@ -34,18 +34,18 @@ var_dump($response);
 ?>
 <?php
 //STEP 3: Generate View URL
-// require 'ReportClient.php';
+require 'ReportClient.php';
 
-// $EMAIL_ID = "rohit@lets-viz.com"; //Email Address
-// $DB_NAME = "Sales Dashboard"; //Workspace Name
-// $TABLE_NAME = "Sales Dashboard";
-// $CLIENT_ID = $ClienID;
-// $CLIENT_SECRET = $ClienSecret;
-// $REFRESH_TOKEN = $_SESSION['refresh_token']; // Get From Step 2
+$EMAIL_ID = "rohit@lets-viz.com"; //Email Address
+$DB_NAME = "Sales Dashboard"; //Workspace Name
+$TABLE_NAME = "Sales Dashboard";
+$CLIENT_ID = $ClienID;
+$CLIENT_SECRET = $ClienSecret;
+$REFRESH_TOKEN = $_SESSION['access_token']; // Get From Step 2
 
-// $report_client_request = new ReportClient($CLIENT_ID, $CLIENT_SECRET, $REFRESH_TOKEN);
+$report_client_request = new ReportClient($CLIENT_ID, $CLIENT_SECRET, $REFRESH_TOKEN);
 
-// $uri = $report_client_request->getURI($EMAIL_ID, $DB_NAME, $TABLE_NAME);
-// $view_url = $report_client_request->getViewUrl($uri);
-$_SESSION['$view_url']=0;
+$uri = $report_client_request->getURI($EMAIL_ID, $DB_NAME, $TABLE_NAME);
+$view_url = $report_client_request->getViewUrl($uri);
+$_SESSION['$view_url']=$view_url;
 ?>
